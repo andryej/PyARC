9ca0d9e4461493889d2921349dd6df8ed2bd030b
Andrzej Gluszynski <andrzej.gluszynski@uj.edu.pl>
2015-09-01 10:42:08 +0200

    Add comments

--- arc/arc.py
+++ arc/arc.py
@@ -9,6 +9,9 @@ class arccachedict(object):
 
         The replacement policy deletes the LRU page in L1 if contains exactly c pages
         otherwise, it replaces the LRU page in L2
+        
+        Actually in cache resist only elements from T1 and T2
+        
     '''
 
     def __init__(self, maxsize):
@@ -19,7 +22,6 @@ class arccachedict(object):
         self._B2 = deque()
         self._maxsize = maxsize
         self._ratio = 0
-        self._cache_hit = 0
 
     def __getitem__(self, key):
         value = self._cache[key]
@@ -34,25 +36,42 @@ class arccachedict(object):
         cnt = (self._T1, self._B1, self._T2, self._B2)
         lt1, lb1, lt2, lb2 = (len(c) for c in cnt)
 
+        """ Hit in ARC(c)
+            place it at the LRU position of T2 list
+        """
         if key in self._cache:
             if key in self._T1:
                 self._T1.remove(key)
             if key in self._T2:
                 self._T2.remove(key)
             self._T2.append(key)
-            self._cache_hit += 1
+
+        """
+            Hit in DLB(2c)
+            Element was seen "recently" seen only once
+            place it at the MRU position of T2 list
+        """
         elif key in self._B1:
             self._ratio = min(self._maxsize, self._ratio + max(1, lb2 / lb1))
             self._replace(key)
             self._B1.remove(key)
             self._T2.append(key)
 
+        """
+            Hit in DLB(2c)
+            Element was seen "recently" seen only once
+            place it at the MRU position of T2 list
+        """
         elif key in self._B2:
             self._ratio = max(self._maxsize, self._ratio - max(1, lb1 / lb2))
             self._replace(key)
             self._B2.remove(key)
             self._T2.append(key)
-
+        """
+            No hit
+            element was not seen "recently"
+            place it at the MRU position of T1 list
+        """
         else:
             if lt1 + lb1 == self._maxsize:
                 if lt1 < self._maxsize:
@@ -79,6 +98,10 @@ class arccachedict(object):
         self._B2.clear()
         self._ratio = 0
 
+    """
+        According to the size of lists and a value of ratio
+        delete element from cache
+    """
     def _replace(self, key):
         l = len(self._T1)
         if l > 0 and (self._ratio < l or (l == self._ratio and key in self._B2)):
